5b7112a1a77a1476194cc39d8a5ab86aeeb1ba03
Farbpent <jgalvezdominguez@gmail.com>
2021-07-17 15:34:23 +0000

    Actualizacion del boton

--- app.js
+++ app.js
@@ -1,46 +1,64 @@
-window.onload = function() {
+window.onload = function () {
     console.log("Hello Rigo from the console!");
 
     DomainNameGenerator();
     document.querySelector("#boton").addEventListener("click", () => {
-        document.querySelector("#Excusa").innerHTML =  RandomDomainNameGenerator()
-      });
-      
-
-  };
-  
-
-  const pronoun = ["the", "our"];
-  const adj = ["great", "big", "Asleep", "Beautiful"];
-  const noun = ["jogger", "racoon", "gamora", "dog", "bird"];
-  const dot = [".es", ".us", ".com", ".pep", ".io"];
+        document.querySelector("#Excusa").innerHTML = RandomDomainNameGenerator()
+    });
 
 
-  function DomainNameGenerator() {
-    
-  
+
+};
+
+
+const pronoun = ["the", "our"];
+const adj = ["great", "big", "Asleep", "Beautiful"];
+const noun = ["jogger", "racoon", "us", "gamora", "dog", "bird"];
+const dot = [".es", ".us", ".com", ".pep", ".io"];
+
+
+function DomainNameGenerator() {
+
+    let dotica = "";
+    let nounico = "";
+
     for (let prona = 0; prona < pronoun.length; prona++) {
-      for (let prone = 0; prone < adj.length; prone++) {
-        for (let proni = 0; proni < noun.length; proni++) {
-          for (let prono = 0; prono < dot.length; prono++) {
-            console.log(pronoun[prona] + adj[prone] + noun[proni] + dot[prono]);
-          }
+        for (let prone = 0; prone < adj.length; prone++) {
+            for (let proni = 0; proni < noun.length; proni++) {
+                for (let prono = 0; prono < dot.length; prono++) {
+                    dotica = dot[prono].split('.');
+
+
+                    if (dotica[1] === dot[prono]) {
+                        console.log(pronoun[prona] + adj[prone] + dot[prono]);
+
+                    }
+                    else {
+
+                        console.log(pronoun[prona].concat(adj[prone], noun[proni], dot[prono]));
+
+                    }
+
+
+
+
+                }
+            }
         }
-      }
     }
-  }
+}
 
-  function RandomDomainNameGenerator(){
+function RandomDomainNameGenerator() {
 
 
-      let dominito = [
+    let dominito = [
         pronoun[Math.floor(Math.random() * pronoun.length)],
         adj[Math.floor(Math.random() * adj.length)],
         noun[Math.floor(Math.random() * noun.length)],
         dot[Math.floor(Math.random() * dot.length)]
-      ];
+    ];
 
-      return dominito.reduce((acc, item) => {
+    return dominito.reduce((acc, item) => {
         return (acc + item);
-      });
-  }
+    });
+}
